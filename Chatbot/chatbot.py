68d9b894105fa81d8e44aa0c68cd373d4fb47ceb
Скіба Євген <y.s.skiba@student.khai.edu>
2021-09-30 10:32:30 +0300

    ChatBot {3-rd} stage

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -1,1 +1,15 @@
-print("Hello! My name is Peter\nI was created in 2021")
+print("Hello! My name is Peter.\nI was created in 2021.")
+print("Please remind me your name")
+while True:
+    user_text = input("Your name")
+    if user_text == 'Zhenya':
+        print("What a great name you have,Zhenya!")
+        break
+    print(user_text)
+
+
+
+
+
+
+
