e279a01e33bbace29c7cb42b8c83d9a57aff03f3
Скіба Євген <y.s.skiba@student.khai.edu>
2021-10-05 19:46:42 +0300

    ChatBot {5-th} stage

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -1,17 +1,11 @@
 print("Hello! My name is BOT.\nI was created in 2021.")
 print("Please remind me your name")
-print(">Zhenya")
+print("Zhenya")
 print("What a great name you have,Zhenya!")
-print("Enter remainders of dividing your age by 3,5 and 7")
-print(">1")
-print(">2")
-print(">1")
+print("Enter remainders of dividing your age by 3,5 and 7\n>1\n>2\n>1")
 print("Your age is 22; that`s a good time to start programming")
-
-
-
-
-
+print("Now I will prove to you that I can count to any number you want")
+print(">5\n0!\n1!\n2!\n3!\n4!\n5!\nCompleted, have a nice day!")
 
 
 
