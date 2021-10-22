65b8ba7e13a70d8b8a9a831f94850a4186f9e94b
Скіба Євген <y.s.skiba@student.khai.edu>
2021-10-22 13:49:12 +0300

    hangman 3 stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,1 +1,9 @@
 print("HANGMAN\nThe game will be available soon.")
+while True:
+    user_text = input("HANGMAN\nGuess the word:")
+    if user_text  == 'python':
+        break
+    else:
+        print("You lost!")
+print("You survived!")
+
