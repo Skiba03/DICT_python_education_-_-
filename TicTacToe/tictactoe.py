5ff51f9d6d518cbad2d38e4887bc137a4f874d51
Скіба Євген <y.s.skiba@student.khai.edu>
2021-12-25 16:12:20 +0300

    TicTacToe 4

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -1,83 +1,86 @@
-b = 0
-c =' '
-t = input("Enter cells:")
-print("---------")
-print("| " + t[0] + ' ' + t[1] + ' ' + t[2] + " |")
-print("| " + t[3] + ' ' + t[4] + ' ' + t[5] + " |")
-print("| " + t[6] + ' ' + t[7] + ' ' + t[8] + " |")
-print("---------")
-a = []
-if str(t[0]) == str(t[1]) == str(t[2]) != c:
-    b += 1
-if str(t[3]) == str(t[4]) == str(t[5]) != c:
-    b += 1
-if str(t[6]) == str(t[7]) == str(t[8]) != c:
-    b += 1
-if str(t[0]) == str(t[3]) == str(t[6]) != c:
-    b += 1
-if str(t[1]) == str(t[4]) == str(t[7]) != c:
-    b += 1
-if str(t[2]) == str(t[5]) == str(t[8]) != c:
-    b += 1
-if str(t[0]) == str(t[4]) == str(t[8]) != c:
-    b += 1
-if str(t[6]) == str(t[4]) == str(t[2]) != c:
-    b += 1
-elif b == 2:
-    print("Impossible")
-    quit()
-if str(t[0]) == str(t[1]) == str(t[2]) != c:
-    print(t[0], "You wins")
-    b += 1
-    quit()
-if str(t[3]) == str(t[4]) == str(t[5]) != c:
-    print(t[3], "You wins")
-    b += 1
-    quit()
-if str(t[6]) == str(t[7]) == str(t[8]) != c:
-    print(t[6], "You wins")
-    b += 1
-    quit()
-if str(t[0]) == str(t[3]) == str(t[6]) != c:
-    print(t[0], "You wins")
-    b += 1
-    quit()
-if str(t[1]) == str(t[4]) == str(t[7]) != c:
-    print(t[1], "You wins")
-    b += 1
-    quit()
-if str(t[2]) == str(t[5]) == str(t[8]) != c:
-    print(t[2], "You wins")
-    b += 1
-    quit()
-if str(t[0]) == str(t[4]) == str(t[8]) != c:
-    print(t[0], "You wins")
-    b += 1
-    quit()
-if str(t[6]) == str(t[4]) == str(t[2]) != c:
-    print(t[6], "You wins")
-    b += 1
-    quit()
-o = t.count('O')
-x = t.count('X')
-if o == 0 and x == 2 or o == 1 and x == 3 or o == 2 and x == 4 or o == 3 and x == 5 or o == 4 and x == 6:
-    print("Impossible")
-    quit()
-if o == 5 and x == 7 or o == 6 and x == 8 or o == 7 and x == 9:
-    print("Impossible")
-    quit()
-if x == 0 and o == 2 or x == 1 and o == 3 or x == 2 and o == 4 or x == 3 and o == 5 or x == 4 and o == 6:
-    print("Impossible")
-    quit()
-if x == 5 and o == 7 or x == 6 and o == 8 or x == 7 and o == 9:
-    print("Impossible")
-    quit()
-if c not in t:
-    if len(a) == 0:
-        print("Draw")
-if c in t and b == 1:
-    if len(a) == 0:
-        print("Game not finished")
-else:
-    print("Game not finished")
-    
+print('Enter cells:')
+a = list(input())
+lin = str('---------')
+b_1 = ['|', a[0], a[1], a[2], '|']
+b_2 = ['|', a[3], a[4], a[5], '|']
+b_3 = ['|', a[6], a[7], a[8], '|']
+s_b = [' ', b_1, b_2, b_3, ' ']
+rang = [int(i) for i in range(1, 4)]
+error1 = 0
+error2 = 0
+
+
+def wins():
+    vvod = []
+    if a.count("X") - a.count("O") >= 2:
+        return "Impossible"
+    elif a.count("O") - a.count("X") >= 2:
+        return "Impossible"
+    if b_1[1] == b_1[2] == b_1[3] != '_':
+        vvod.append(b_1[1])
+    if b_2[1] == b_2[2] == b_2[3] != '_':
+        vvod.append(b_2[1])
+    if b_3[1] == b_3[2] == b_3[3] != '_':
+        vvod.append(b_3[1])
+    if b_1[1] == b_2[1] == b_3[1] != '_':
+        vvod.append(b_1[1])
+    if b_1[2] == b_2[2] == b_3[2] != '_':
+        vvod.append(b_1[2])
+    if b_1[3] == b_2[3] == b_3[3] != '_':
+        vvod.append(b_1[3])
+    if b_1[1] == b_2[2] == b_3[3] != '_':
+        vvod.append(b_1[1])
+    if b_1[3] == b_2[2] == b_3[1] != '_':
+        vvod.append(b_1[3])
+    if vvod.count('X') >= 2:
+        vvod.remove('X')
+    elif vvod.count('O') >= 2:
+        vvod.remove('O')
+    if len(vvod) >= 2:
+        return "Impossible"
+    elif len(vvod) == 1:
+        return vvod[0] + ' wins'
+    elif '_' not in a:
+        if len(vvod) == 0:
+            return "Draw"
+    elif '_' in a:
+        if len(vvod) == 0:
+            return "Game not finished"
+
+
+def pole():
+    print(lin)
+    print(' '.join(b_1))
+    print(' '.join(b_2))
+    print(' '.join(b_3))
+    print(lin)
+    print(wins())
+
+
+def error():
+    c = 0
+    global b_1, b_2, b_3, error1, error2
+    while c != 1:
+        print('Enter the coordinates:')
+        try:
+            error1, error2 = input().split(' ')
+            error1 = int(error1)
+            error2 = int(error2)
+        except ValueError:
+            print('You should enter numbers!')
+            continue
+        if error1 and error2 not in rang:
+            print('Coordinates should be from 1 to 3! Example: 1 2, 2 3')
+        elif error1 and error2 in rang:
+            if s_b[error1][error2] != '_':
+                print('This cell is occupied! Choose another one!')
+            else:
+                s_b[int(error1)][int(error2)] = 'X'
+                c = 1
+    pole()
+
+
+pole()
+
+
+error()
