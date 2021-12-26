d1710cc7666d27cee747f38954dca2e842d890a1
Скіба Євген <y.s.skiba@student.khai.edu>
2021-12-26 21:12:08 +0300

    TicTacToe 5

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -1,3 +1,8 @@
+print('---------')
+print("| _ _ _ |")
+print("| _ _ _ |")
+print("| _ _ _ |")
+print('---------')
 print('Enter cells:')
 a = list(input())
 lin = str('---------')
@@ -8,6 +13,7 @@ s_b = [' ', b_1, b_2, b_3, ' ']
 rang = [int(i) for i in range(1, 4)]
 error1 = 0
 error2 = 0
+v = 0
 
 
 def wins():
@@ -59,28 +65,47 @@ def pole():
 
 def error():
     c = 0
-    global b_1, b_2, b_3, error1, error2
+    global b_1, b_2, b_3, error1, error2, v, a
     while c != 1:
+        v += 1
         print('Enter the coordinates:')
         try:
             error1, error2 = input().split(' ')
             error1 = int(error1)
             error2 = int(error2)
         except ValueError:
+            v -= 1
             print('You should enter numbers!')
             continue
         if error1 and error2 not in rang:
-            print('Coordinates should be from 1 to 3! Example: 1 2, 2 3')
+            print('Coordinates should be from 1 to 3!')
+            v -= 1
         elif error1 and error2 in rang:
             if s_b[error1][error2] != '_':
+                v -= 1
                 print('This cell is occupied! Choose another one!')
             else:
-                s_b[int(error1)][int(error2)] = 'X'
-                c = 1
+                if v % 2 == 1:
+                    s_b[int(error1)][int(error2)] = 'X'
+                    if error1 == 1:
+                        a[error2 - error1] = 'X'
+                    elif error1 == 2:
+                        a[error1 + error2] = 'X'
+                    else:
+                        a[error1 + error2 + 2] = 'X'
+                    c = 1
+                else:
+                    s_b[int(error1)][int(error2)] = 'O'
+                    if error1 == 1:
+                        a[error2 - error1] = 'O'
+                    elif error1 == 2:
+                        a[error1 + error2] = 'O'
+                    else:
+                        a[error1 + error2 + 2] = 'O'
+                    c = 1
     pole()
 
 
 pole()
-
-
-error()
+while wins() == 'Game not finished':
+    error()
