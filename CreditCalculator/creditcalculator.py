74ee1913888bbbee33ce6ebb6214dae8097c836d
Скіба Євген <y.s.skiba@student.khai.edu>
2021-12-27 13:19:13 +0300

    Credit Calculator 4

--- CreditCalculator/creditcalculator.py
+++ CreditCalculator/creditcalculator.py
@@ -1,4 +1,17 @@
 import math
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--type', type=str)
+parser.add_argument('--principal', type=int)
+parser.add_argument('--payment', type=int)
+parser.add_argument('--interest', type=float)
+parser.add_argument('--periods', type=int)
+parser.add_argument('--annuity', type=float)
+
+arguments = parser.parse_args()
+
 
 def year(l, p, a):
     i = (l * 0.01 / 12)
@@ -7,63 +20,85 @@ def year(l, p, a):
     return a * ((i * num) / (num - 1))
 
 
-def a(l, q, p):
+def al(l, ann, p):
     i = (l * 0.01 / 12)
     num = pow(1 + i, p)
 
-    return q / ((i * num) / (num - 1))
+    return ann / ((i * num) / (num - 1))
 
 
-def t(l, m, a):
+def payment(l, mp, a):
     i = (l * 0.01 / 12)
-    A = (m / (m - i * a))
+    A = (mp / (mp - i * a))
 
     return math.ceil(math.log(A, i + 1))
 
 
-def n():
-    a = int(input('Enter the loan principal: '))
-    payment = int(input('Enter the monthly payment: '))
-    a_interest = int(input('Enter the loan interest: '))
+def diff(l, per, a):
+    i = (l * 0.01 / 12)
+    mon = 0
+    res = 0
+    s = list(reversed(range(2, per + 2)))
+    for monthly in s:
+        monthly -= 1
+        d = math.ceil(a / per) + i * ((a * monthly) / per)
+        res += d
+        mon += 1
+        print(f"Month {mon}: payment is {math.ceil(d)}")
+    print('Overpayment {}'.format(math.ceil(res - a)))
 
-    n = t(a_interest, payment, a)
+
+def n(pr, pay, i):
+    n = payment(i, pay, pr)
 
     years = n / 12
     month = (years - math.floor(years)) * 12
-    print('It will take {} years and {} months to repay this loan!'.format(math.floor(years), math.floor(month)))
+    if math.floor(month) != 0:
+        print('It will take {} years and {} months to repay this loan!'.format(math.floor(years), math.ceil(month)))
+    else:
+        print('It will take {} years to repay this loan!'.format(math.ceil(years)))
+    print('Overpayment {}'.format(int(pr * (1 + i * 0.01 / 0.75) - pr)))
 
 
-def ab():
-    a = int(input('Enter the loan principal: '))
-    periods = int(input('Enter the number of periods: '))
-    a_interest = int(input('Enter the loan interest: '))
-
-    payment = math.ceil(year(a_interest, periods, a))
+def a(a, per, i):
+    payment = math.ceil(year(i, per, a))
 
     print('Your monthly payment = {}!'.format(payment))
+    print('Overpayment {}'.format(math.ceil(payment) * per - a))
 
 
-def p():
-    annuity_payment = float(input('Enter the annuity payment: '))
-    periods = int(input('Enter the number of periods: '))
-    a_interest = float(input('Enter the loan interest: '))
+def p(pay, per, i):
+    loan_principal = math.floor(al(i, pay, per))
 
-    principal = math.floor(a(a_interest, annuity_payment, periods))
-
-    print('Your loan principal = {}!'.format(principal))
-
+    print('Your loan principal = {}!'.format(loan_principal))
+    print('Overpayment {}'.format((pay * per - i)))
 
 
 def CreditCalculator():
-    z = input('What do you want to calculate?\ntype "n" for number of monthly payments,\ntype "a" for annuity monthly payment amount,\ntype "p" for loan principal:')
-
-
-    if z == 'n':
-        n()
-    elif z == 'a':
-        ab()
-    elif z == 'p':
-        p()
+    try:
+        if arguments.type == 'diff':
+            if arguments.interest and arguments.periods and arguments.principal:
+                diff(arguments.interest, arguments.periods, arguments.principal)
+        elif arguments.type == 'annuity':
+            if arguments.payment and arguments.principal:
+                if arguments.principal != 0 and arguments.payment != 0 and arguments.interest != 0:
+                    n(arguments.principal, arguments.payment, arguments.interest)
+                else:
+                    print("Incorrect parameters")
+            elif arguments.principal and arguments.periods:
+                if arguments.principal != 0 and arguments.periods != 0 and arguments.interest != 0:
+                    a(arguments.principal, arguments.periods, arguments.interest)
+                else:
+                    print("Incorrect parameters")
+            elif arguments.payment:
+                if arguments.payment != 0 and arguments.periods != 0 and arguments.interest != 0:
+                    p(arguments.payment, arguments.periods, float(arguments.interest))
+                else:
+                    print("Incorrect parameters")
+        else:
+            print("Incorrect parameters")
+    except TypeError:
+        print("Incorrect parameters")
 
 
 CreditCalculator()
