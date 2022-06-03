b5f1ca73d35e2af41cf6866631697a66acea83c4
Zhenya <y.s.skiba@student.khai.edu>
2022-06-03 16:22:42 +0300

    Arithmetic_test1

---
+++ arithmetic_test.py
@@ -0,0 +1,48 @@
+from random import choice
+
+znak = 0.
+a = 0
+b = 0
+c = 0
+d = 1
+
+
+def stage_1():
+    global a, b, c
+    if d == 1:
+        a = choice(range(2, 10))
+        b = choice(range(2, 10))
+        c = choice(['+', '-', '*'])
+
+
+def stage1():
+    stage_1()
+    global znak, a, b, c
+    if c == '+':
+        print(str(a) + '+' + str(b))
+        znak = a + b
+    elif c == '-':
+        print(str(a) + '-' + str(b))
+        znak = a - b
+    elif c == '*':
+        print(str(a) + '*' + str(b))
+        znak = a * b
+
+
+def otvet():
+    global d
+    if znak == answer:
+        print("Right")
+        d = 1
+    else:
+        print("Wrong")
+        d = 1
+
+
+try:
+    stage1()
+    answer = int(input())
+    otvet()
+except ValueError:
+    print('Incorrect format')
+    d = 0
